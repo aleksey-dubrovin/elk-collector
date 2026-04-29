8cbc56f412c1f604688cff5a227b489ab6f49fb6
Aleksey Dubrovin <dualvd@gmail.com>
2026-04-29 20:47:07 +0500

    Добавлена конфигурация с одним узлом

--- pinger/run.py
+++ pinger/run.py
@@ -3,17 +3,29 @@
 import logging
 import random
 import time
+import sys
 
-while True:
-    number = random.randrange(0, 4)
+# Настройка вывода логов в stdout
+logging.basicConfig(
+    stream=sys.stdout,
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s'
+)
 
-    if number == 0:
-        logging.info('Hello there!!')
-    elif number == 1:
-        logging.warning('Hmmm....something strange')
-    elif number == 2:
-        logging.error('OH NO!!!!!!')
-    elif number == 3:
-        logging.exception(Exception('this is exception'))
-
-    time.sleep(1)
+if __name__ == "__main__":
+    while True:
+        number = random.randrange(0, 4)
+        
+        if number == 0:
+            logging.info('Hello there!!')
+        elif number == 1:
+            logging.warning('Hmmm....something strange')
+        elif number == 2:
+            logging.error('OH NO!!!!!!')
+        elif number == 3:
+            try:
+                raise Exception('this is exception')
+            except Exception as e:
+                logging.exception(e)
+        
+        time.sleep(1)
